b16d90d43ea451c8ac6a0567dac4ea49f189ae76
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-04-06 19:58:59 +0300

    Postcard updtes

--- src/components/layout/PostCard.js
+++ src/components/layout/PostCard.js
@@ -3,10 +3,16 @@ import RightSideBarUserCard from './RightSideBarUserCard'
 import Postimg from '../../assets/page/profile.png'
 const PostCard = (props) => {
   return (
-    <div>
-   <RightSideBarUserCarderCard username="John Doe"  status="7 hours, 57 minutes ago"/>
-   <h1 className="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, harum quisquam. Consequuntur et harum fugiat laborum deleniti quis autem. Deleniti nihil accusamus unde assumenda. Iusto quia autem officia porro vitae?</h1>
-   <img src={Postimg} alt="there should have been a post here" className="my-2 bg-contain bg-center h-9 w-9 object-cover rounded"/>
+    <div className=" box-content justify-start">
+   <RightSideBarUserCard username="John Does" 
+   status="7 hours, 57 minutes ago"/>
+<div className=" px-9">
+   <h1 className="text-ellipsis text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, harum quisquam. Consequuntur et harum fugiat laborum deleniti quis autem. Deleniti nihil accusamus unde assumenda. Iusto quia autem officia porro vitae?</h1>
+   <img src={Postimg} alt="there should have been a post here"
+    className="my-2 bg-contain
+     bg-center object-cover 
+     rounded h-50"/>
+   </div>
    </div>
   )
 }
