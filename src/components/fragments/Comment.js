6e4caaafba56891ff5ce5d0e35b1eddd056911b0
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-04-07 17:49:18 +0300

    Comment added

--- src/components/fragments/Comment.js
+++ src/components/fragments/Comment.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useState} from 'react'
 import styled from 'styled-components'
 import Profile from '../../assets/page/profile.png'
-
+import {Link}from 'react-router-dom'
 const Comment = (props) => {
   return(<div>
       <PlaceHolder className="flex flex-row">
@@ -11,15 +11,18 @@ const Comment = (props) => {
   
     </PlaceHolder>
     <PlaceHolder className="flex flex-row">
+    <Link to="/profile/:username" className="flex flex-row">
     <img src={Profile} className="my-3 mx-3 bg-contain 
     bg-center h-9 w-9 
     object-cover rounded-3xl"/>
+    
     <h4 className="pt-4 text-base font-normal 
   font-sans antialiased 
   text-base border-b-[1px] 
 border-slate-200  hover:border-b-[1px]  
-  hover:border-blue-500">{props.username}</h4>
+  hover:border-blue-500">{props.username}</h4></Link>
 <p className='pt-4 px-2' >{props.status}</p>
+
     </PlaceHolder>
     <p className="my-1 mx-14">{props.comment}</p>
       </div>
