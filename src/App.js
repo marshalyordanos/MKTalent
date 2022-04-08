a8c4efd21d7517b33bd141368805f4ad0ff8d2f9
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-04-08 18:57:08 +0300

    Event page card updated

--- src/App.js
+++ src/App.js
@@ -5,15 +5,29 @@ import Layout from './components/layout/LayoutApp'
 import JobPage from './pages/JobPage';
 import VideoPage from './pages/VideoPage';
 import JobDetail from './components/fragments/job/JobDetail';
-
+import PostCard from './components/fragments/PostCard'
+import EventDetail from './components/fragments/event/EventDetail';
+import EventCard from './components/fragments/event/EventCard';
 function App() {
   return (
     <div className="App">
       {/* <HomeApp/> */}
       <Layout>
         {/* <JobPage/> */}
-        <JobDetail/>
+        {/* <JobDetail/> */}
         {/* <VideoPage/> */}
+        <EventDetail/>
+        {/* <EventCard/>
+        <EventCard/>
+        <EventCard/>
+        <EventCard/>
+        <EventCard/>
+        <EventCard/>
+        <EventCard/> */}
+        {/* <PostCard/>
+        <PostCard/>
+        <PostCard/>
+        <PostCard/> */}
       </Layout>
       {/* zklnc */}
     </div>
