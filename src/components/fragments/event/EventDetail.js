ff6c01762544fc75bbe49bc097c0469ff40cdfc8
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-04-21 09:30:25 +0300

    logo redesigned

--- src/components/fragments/event/EventDetail.js
+++ src/components/fragments/event/EventDetail.js
@@ -38,7 +38,7 @@ const EventDetail = () => {
     ]
   return (
     <EventDetailStyle>
-        {/* <EventCard/> */}
+        <EventCard/>
         <div className='eventDetail__type text-gray-500'>
             <div className='eventDetail__type1 '>
                 <div className='a1'>
