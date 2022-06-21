f9cc8e1bdd671c4d4f73eb96043a7e66404a1e2f
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-06-22 01:37:36 +0300

    admin page updates

--- src/components/admin/dashboard/Dashboard.js
+++ src/components/admin/dashboard/Dashboard.js
@@ -18,7 +18,7 @@ const Dashboard = (props) => {
          <div className="home">
       {/* <Sidebar /> */}
       <div className="homeContainer">
-        <Navbar />
+        {/* <Navbar /> */}
         <div className="widgets">
           <Widget type="user" />
           <Widget type="order" />
