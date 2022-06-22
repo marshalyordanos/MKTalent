8f86f99bec1a2685a6672a59e11a9be9540b633e
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-06-22 20:22:05 +0300

    blog page routing updates

--- src/components/admin/dashboard/Dashboard.js
+++ src/components/admin/dashboard/Dashboard.js
@@ -9,10 +9,36 @@ import Widget from "../widget/Widget";
 import Featured from "../featured/Featured";
 import Chart from "../chart/Chart";
 import Table from "../table/Table";
+import { userColumns, userRows } from "../datatablesource";
+
+import { useEffect,  useMemo } from "react";
+import api from "../../../api/api";
+
 const Dashboard = (props) => {
   const dispatch = useDispatch();
   const light = useSelector((state) => state.mode.light);
-
+  const MONTHS = useMemo(()=>[   
+     "January", 
+  "February" ,
+  "March", 
+   "April",
+   "May" , 
+   "June"]) 
+  
+  // const Chart = ({ aspect, title }) => 
+  //   const [data, setData] = useState(userRows);
+  // const [searchUser, setSearchUser] = useState([]);
+  const [users, setUsers] = useState([]);
+  
+    // useEffect(() => {
+    //   const feachData = async () => {
+    //     const users = await api.get("/profile");
+    //     console.log("marshalwwwwwwwwwwwwww", users.data.data);
+    //     users.data.data.map(item=>setUsers(prev=>[...prev,{name:MONTHS[item._id-1], "New User":item.createdAt}]))
+    //     setSearchUser(users.data.data);
+    //   };
+    //   feachData();
+    // }, [MONTHS]);
   return (
     <div>
          <div className="home">
@@ -28,7 +54,7 @@ const Dashboard = (props) => {
         </div>
         <div className="charts">
           <Featured />
-          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+          <Chart data={users} daraKey="New User"title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
         <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
@@ -38,6 +64,5 @@ const Dashboard = (props) => {
     </div>
     </div>
   );
-};
-
+}
 export default Dashboard;
