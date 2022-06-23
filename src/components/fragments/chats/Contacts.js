02d873abf9e085164e4ede2fddcc50ebd1eb9200
marshalyordanos <marshalyordanosseifu@gmail.com>
2022-06-23 04:11:21 +0300

    some

--- src/components/fragments/chats/Contacts.js
+++ src/components/fragments/chats/Contacts.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 
 const Contacts = ({ contacts, changeChat }) => {
   const [currentSelected, setCurrentSelected] = useState(undefined);
+  const [name, setName] = useState("");
 
+  const [searchUser, setSearchUser] = useState(contacts);
+  const [users, setUsers] = useState([]);
   const changeCurrentChat = (index, contact) => {
     setCurrentSelected(index);
     changeChat(contact);
+  };
+  const handleChange = (e) => {
+    // setSearch(e.target.value);
+    const xx = contacts.filter((user) =>
+      user.user.username.toLowerCase().startsWith(e.target.value.toLowerCase())
+    );
+    console.log("pppppp", xx);
+    setSearchUser(xx);
   };
   return (
     <div class="col-12 col-lg-5 col-xl-3 border-right">
@@ -13,6 +24,7 @@ const Contacts = ({ contacts, changeChat }) => {
         <div class="d-flex align-items-center">
           <div class="flex-grow-1">
             <input
+              onChange={handleChange}
               type="text"
               class="form-control my-3"
               placeholder="Search..."
@@ -21,7 +33,7 @@ const Contacts = ({ contacts, changeChat }) => {
         </div>
       </div>
 
-      {contacts.map((user, i) => {
+      {searchUser.map((user, i) => {
         return (
           <div onClick={() => changeCurrentChat(i, user)}>
             <a href="#" class="list-group-item list-group-item-action border-0">
